f1af3e8bb5e9c5034ae012fea946ef55b409a4e5
LeonimusTTV <65367018+LeonimusTTV@users.noreply.github.com>
2020-12-26 16:47:27 +0100

    Update message.js

--- Events/message.js
+++ Events/message.js
@@ -4,7 +4,7 @@ const prefix = "l!";
 module.exports = async(client, message) => {
 
     if(message.author.bot) return;
-    if(message.channel.type === "dm") return message.channel.send("Le bot ne répond pas au message/commande on message privé <:thisshitgood:702100590448082954>");
+    //if(message.channel.type === "dm") return message.channel.send("Le bot ne répond pas au message/commande on message privé <:thisshitgood:702100590448082954>");
 
     if(!message.content.startsWith(prefix)) return;
 
